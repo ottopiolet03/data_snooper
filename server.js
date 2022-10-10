a90f04dcd5de249c791057e5c63a69502c1d3bbe
ottopiolet03 <momajtenyi@gmail.com>
2022-10-10 18:35:27 -0400

    added connection to gremlin

--- server.js
+++ server.js
@@ -1,14 +1,56 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-const port = process.env.PORT || 8080;
+const Gremlin = require('gremlin');
+const config = require('./wwwroot/js/config');
 
-app.use(express.static(__dirname + "/wwwroot"));
-// sendFile will go here
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+
+//set up functions to recieve data from Cosmos DB
+const authenticator = new Gremlin.driver.auth.PlainTextSaslAuthenticator(`/dbs/${config.database}/colls/${config.collection}`, config.primaryKey)
+
+const client = new Gremlin.driver.Client(
+    config.endpoint,
+    {
+        authenticator,
+        traversalsource: "g",
+        rejectUnauthorized: true,
+        mimeType: "application/vnd.gremlin-v2.0+json"
+    }
+);
+
+
+let vertices;
+client.submit("g.V()", {}).then((vertex_res) => {
+    vertices = vertex_res;
+    client.submit("g.E()", {}).then((edge_result) => {
+        let edges = edge_result;
+        let datae = {vertices, edges };
+        console.log(datae);
+        const app = express();
+        const port = process.env.PORT || 8080;
+
+        app.use(express.static(__dirname + "/wwwroot"));
+        // sendFile will go here
+
+
+        app.get('/', function (req, res) {
+            res.sendFile(path.join(__dirname, '/index.html'));
+        });
+        app.get('/data', function (req, res) {
+            res.send(datae);
+        });
+
+        app.listen(port);
+        console.log('Server started at http://localhost:' + port);
+    });
 });
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+
+
+
+
+
+
+
+
+
