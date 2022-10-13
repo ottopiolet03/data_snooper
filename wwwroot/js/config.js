c1ff49a7f225f9a71c4ccf27b14d050aefd5da81
ottopiolet03 <115287791+ottopiolet03@users.noreply.github.com>
2022-10-13 09:35:08 -0400

    Delete config.js

--- wwwroot/js/config.js
+++
@@ -1,8 +0,0 @@
-const config = {};
-
-config.endpoint = "wss://data-snoop-graph.gremlin.cosmos.azure.com:443/";
-config.primaryKey = "722PKJscB21M8s6r50AwjraoygUJYJkiVcVJ5IQDMUUJcJNo7AVHiiED72sC6zWhSferC9VvnTY39gon7O214A==";
-config.database = "graph3"
-config.collection = "graph5"
-
-module.exports = config;
